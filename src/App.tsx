c7fed76610a872700e60162eca8e0c595fe5e4c5
Chan Park <chanpark0202@gmail.com>
2025-03-07 22:26:30 +0900

    Fix CSP error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { LedgerWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -12,12 +11,7 @@ function App() {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(() => [
-    new PhantomWalletAdapter(),
-    new TorusWalletAdapter(),
-    new LedgerWalletAdapter(),
-    new SolflareWalletAdapter(),
-  ], [network]);
+  const wallets = useMemo(() => [], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
