c2d18fd2485bc459be640b20c6bac1c290cb1747
Chan Park <chanpark0202@gmail.com>
2025-03-07 22:30:22 +0900

    Ledger adapter was a bad guy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { 
+  PhantomWalletAdapter,
+  TorusWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -11,7 +16,11 @@ function App() {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(() => [], [network]);
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter(),
+    new TorusWalletAdapter(),
+    new SolflareWalletAdapter(),
+  ], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
