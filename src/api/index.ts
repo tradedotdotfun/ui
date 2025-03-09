ac286b42023beece464807704e39728b8c2178a0
Chan Park <chanpark0202@gmail.com>
2025-03-10 02:00:21 +0900

    replace api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://3.39.177.101:8080/"
+const API_BASE_URL = "https://api.tradedot.fun/"
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
