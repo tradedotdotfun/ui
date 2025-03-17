a900072d605587b21a0b761567874965df1228a6
this-is-iron <iron@superblock.xyz>
2025-03-17 20:45:32 +0900

    Fix bug

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -153,8 +153,8 @@ export default function MainLogo() {
           <>
             <img src="/pepe-box.gif" alt="pepe-box" className="w-[180px] absolute top-[196px] left-[calc(50%-606px)]" />
             <img src="/pepe-punch.gif" alt="pepe-punch" className="w-[180px] absolute top-[240px] right-[calc(50%-606px)] transform -scale-x-100" />
-            <img src="/pepe-dance.gif" alt="pepe-dance" className="w-[215px] absolute top-[410px] left-[calc(50%-380px)]" />
-            <img src="/sonic-dancing.gif" alt="sonic-dancing" className="w-[113px] absolute top-[480px] right-[calc(50%-280px)]" />
+            <img src="/pepe-dance.gif" alt="pepe-dance" className="w-[215px] absolute top-[400px] left-[calc(50%-380px)]" />
+            <img src="/sonic-dancing.gif" alt="sonic-dancing" className="w-[113px] absolute top-[470px] right-[calc(50%-280px)]" />
           </>
         )
       }
